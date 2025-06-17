59fd7e591f7b7341bbdb690eb0024ff0c95efe5d
zeviraty <zeviraty@gmail.com>
2025-06-17 18:31:39 +0200

    Added logs to "logging.py"

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,11 +1,21 @@
+from datetime import datetime
+
 def warn(text:str, name:str="+"):
-    print(f"\033[33m[WARN][{name}] {text}\033[0m")
+    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[33m[WARN] [{name}] {text}"
+    open('logs/'+datetime.today().strftime('%Y-%m-%d'),'a').write(text.replace("\033[33m")+"\n")
+    print(text+"\033[0m")
 
 def error(text:str, name:str="+"):
-    print(f"\033[31m[ERROR][{name}] {text}\033[0m")
+    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[31m[ERROR] [{name}] {text}"
+    open('logs/'+datetime.today().strftime('%Y-%m-%d'),'a').write(text.replace("\033[31m")+"\n")
+    print(text+"\033[0m")
 
 def info(text:str, name:str="+"):
-    print(f"[INFO][{name}] {text}\033[0m")
+    text = f"[{datetime.today().strftime('%H:%M:%S')}] [INFO] [{name}] {text}"
+    open('logs/'+datetime.today().strftime('%Y-%m-%d'),'a').write(text+"\n")
+    print(text+"\033[0m")
 
 def disconnect(text:str, name:str="+"):
-    print(f"\033[34m[DISCONNECT][{name}] {text}\033[0m")
+    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[34m[DISCONNECT] [{name}] {text}"
+    open('logs/'+datetime.today().strftime('%Y-%m-%d'),'a').write(text.replace("\033[34m")+"\n")
+    print(text+"\033[0m")
