505b0a350fa22ac7ec062b128a03335ed5c46af1
zeviraty <zeviraty@gmail.com>
2025-07-10 15:41:45 +0200

    Turned main.py into a cli

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from player import Player
 from utils.color import RESET
 import db
 import utils.logging as log
+import click
 
 clients: list[Player] = []
 global id
@@ -27,7 +28,7 @@ def handle_client(client_socket, addr, id):
         client.username = "zevvi"
     client.mainmenu()
 
-def main():
+def main(server):
     global id
     while True:
         client, addr = server.accept()
@@ -39,17 +40,21 @@ def main():
         except BrokenPipeError:
             log.disconnect(f"{addr[0]}:{addr[1]} broke pipe" )
 
-if __name__ == "__main__":
-    bind_ip = "0.0.0.0"
-    bind_port = 2323
+@click.command()
+@click.option('--bind', default="0.0.0.0", help="IP to bind to")
+@click.option('--port', default=2323, help="Port for tome")
+def cmd(bind = "0.0.0.0", port = 2323):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((bind_ip, bind_port))
+    server.bind((bind, port))
     server.listen(5)
-    log.info(f"Listening on port {bind_ip} : {bind_port}")
+    log.info(f"Listening on port {bind} : {port}")
     try:
-        main()
+        main(server)
     except KeyboardInterrupt:
         log.info("Closing server...")
         for client in clients:
             client.disconnect("Server closed by admin.")
+
+if __name__ == "__main__":
+    cmd()
