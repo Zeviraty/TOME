3702c71277f7414ad23583b06fd280273cc55106
zeviraty <zeviraty@gmail.com>
2025-07-11 21:54:11 +0200

    Improved debug mode.

--- src/main.py
+++ src/main.py
@@ -6,35 +6,35 @@ from utils.color import RESET
 import db
 import utils.logging as log
 import click
+import os
 
 clients: list[Player] = []
 global id
 id = 0
 
-DEBUG = True
-
-def handle_client(client_socket, addr, id):
+def handle_client(client_socket, addr, id,debug):
     client = Player(client_socket, addr, id)
     clients.append(client)
     client.send(RESET)
     client.send(banners.generate("TOME"))
     client.send("Welcome to TOME!\n")
-    if not DEBUG:
+    if not debug:
         client.login()
     else:
         conn = db.get()
-        client.user = conn.execute("SELECT * FROM accounts WHERE name = ?;",("zevvi",)).fetchone()
-        client.mudclient = {'client': 'Mudlet', 'version': '4.19.1'}
-        client.username = "zevvi"
+        debug_player = os.getenv("TOME_DEBUG_USER")
+        if debug_player == None:
+            debug_player = "admin"
+        client.login(debug_player)
     client.mainmenu()
 
-def main(server):
+def main(server,debug):
     global id
     while True:
         client, addr = server.accept()
         log.info(f"Accepted connection from: {addr[0]}:{addr[1]} id: {id + 1}")
         id += 1
-        client_handler = threading.Thread(target=handle_client, args=(client,addr,id))
+        client_handler = threading.Thread(target=handle_client, args=(client,addr,id,debug))
         try:
             client_handler.start()
         except BrokenPipeError:
@@ -43,14 +43,15 @@ def main(server):
 @click.command()
 @click.option('--bind', default="0.0.0.0", help="IP to bind to")
 @click.option('--port', default=2323, help="Port for tome")
-def cmd(bind = "0.0.0.0", port = 2323):
+@click.option('--debug', help="Enable debug mode", is_flag=True)
+def cmd(bind = "0.0.0.0", port = 2323, debug=False):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((bind, port))
     server.listen(5)
     log.info(f"Listening on port {bind} : {port}")
     try:
-        main(server)
+        main(server,debug)
     except KeyboardInterrupt:
         log.info("Closing server...")
         for client in clients:
