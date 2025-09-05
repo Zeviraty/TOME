93116da03f8944653778bb9c44c90e442efbc1f5
zeviraty <zeviraty@gmail.com>
2025-09-05 10:56:20 +0200

    Changed text -> data in logging.py

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -22,42 +22,42 @@ def start():
         else:
             current_log += "1"
 
-def warn(text:str, name:str="+"):
-    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[33m[WARN] [{name}] {text}"
-    open('logs/'+current_log,'a').write(text.replace("\033[33m",'')+"\n")
-    print(text+"\033[0m")
+def warn(data, name:str="+"):
+    data = f"[{datetime.today().strftime('%H:%M:%S')}] \033[33m[WARN] [{name}] {data}"
+    open('logs/'+current_log,'a').write(data.replace("\033[33m",'')+"\n")
+    print(str(data)+"\033[0m")
 
-def error(text:str, name:str="+"):
-    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[31m[ERROR] [{name}] {text}"
-    open('logs/'+current_log,'a').write(text.replace("\033[31m",'')+"\n")
-    print(text+"\033[0m")
+def error(data, name:str="+"):
+    data = f"[{datetime.today().strftime('%H:%M:%S')}] \033[31m[ERROR] [{name}] {data}"
+    open('logs/'+current_log,'a').write(data.replace("\033[31m",'')+"\n")
+    print(str(data)+"\033[0m")
 
-def info(text:str, name:str="+"):
-    text = f"[{datetime.today().strftime('%H:%M:%S')}] [INFO] [{name}] {text}"
-    open('logs/'+current_log,'a').write(text+"\n")
-    print(text+"\033[0m")
+def info(data, name:str="+"):
+    data = f"[{datetime.today().strftime('%H:%M:%S')}] [INFO] [{name}] {data}"
+    open('logs/'+current_log,'a').write(str(data)+"\n")
+    print(str(data)+"\033[0m")
 
-def disconnect(text:str, name:str="+"):
-    text = f"[{datetime.today().strftime('%H:%M:%S')}] \033[34m[DISCONNECT] [{name}] {text}"
-    open('logs/'+current_log,'a').write(text.replace("\033[34m",'')+"\n")
-    print(text+"\033[0m")
+def disconnect(data, name:str="+"):
+    data = f"[{datetime.today().strftime('%H:%M:%S')}] \033[34m[DISCONNECT] [{name}] {data}"
+    open('logs/'+current_log,'a').write(data.replace("\033[34m",'')+"\n")
+    print(str(data)+"\033[0m")
 
 
 
-def pwarn(text:str, name:str="+"):
+def pwarn(data, name:str="+"):
     prefix = f"[{datetime.today().strftime('%H:%M:%S')}] \033[33m[WARN] [{name}]"
-    open('logs/'+current_log,'a').write(text.replace("\033[33m",'')+"\n")
+    open('logs/'+current_log,'a').write(data.replace("\033[33m",'')+"\n")
     print(prefix,end="")
-    pp(text,"\033[0m")
+    pp(data,"\033[0m")
 
-def perror(text:str, name:str="+"):
+def perror(data, name:str="+"):
     prefix = f"[{datetime.today().strftime('%H:%M:%S')}] \033[31m[ERROR] [{name}]"
-    open('logs/'+current_log,'a').write(text.replace("\033[31m",'')+"\n")
+    open('logs/'+current_log,'a').write(data.replace("\033[31m",'')+"\n")
     print(prefix,end="")
-    pp(text,"\033[0m")
+    pp(data,"\033[0m")
 
-def pinfo(text:str, name:str="+"):
+def pinfo(data, name:str="+"):
     prefix = f"[{datetime.today().strftime('%H:%M:%S')}] [INFO] [{name}]"
-    open('logs/'+current_log,'a').write(text+"\n")
+    open('logs/'+current_log,'a').write(str(data)+"\n")
     print(prefix,end="")
-    pp(text,"\033[0m")
+    pp(data,"\033[0m")
