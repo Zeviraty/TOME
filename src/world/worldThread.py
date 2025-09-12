ef9f2fa60f3087c51106b36a31d04542c0f60b76
zeviraty <zeviraty@gmail.com>
2025-09-12 14:14:17 +0200

    Created worldthread

---
+++ src/world/worldThread.py
@@ -0,0 +1,21 @@
+import world.map as m
+import sending
+import utils.logging as log
+
+global world_map
+world_map = m.Map
+
+def worldThread():
+    while True:
+        recv = sending.receive("World")
+        if type(recv) == sending.Message:
+            if type(recv.message) != dict or recv.message.keys() != ["command","data"]:
+                log.warn(f"Got a message from: {recv.sender_id} that wasnt the right format: {recv.message}","World")
+            else:
+                match recv.message["command"]:
+                    case "get_map":
+                        if recv.message["data"].keys() != ["x", "y", "z","layer"]:
+                            log.warn(f"Got a message from: {recv.sender_id} that wasnt the right format: {recv.message}","World")
+                        else:
+                            print("getting map TODO")
+        pass
