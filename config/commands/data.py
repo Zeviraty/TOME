111270a382f9ad6163bb9e5d25400466ed779aa9
zeviraty <zeviraty@gmail.com>
2025-11-09 18:37:31 +0100

    Made commands configurable

---
+++ config/commands/data.py
@@ -0,0 +1,7 @@
+import sending
+
+def map(client,arguments):
+    msg = sending.Message("World",client.td,{"command":"get_map","data":{"map":"Overworld","x":0,"y":0}})
+    sending.send(msg)
+    msg = sending.receive_block(client.td)
+    client.send(msg)
--- src/client/game.py
+++ src/client/game.py
@@ -1,10 +1,26 @@
 import sending
+import utils.import_lib as il
+import utils.config as conf
+import utils.logging as log
+import re
 
 def parse_command(cmd, client):
-    match cmd:
-        case "map":
-            msg = sending.Message("World",client.td,{"command":"get_map","data":{"x":0,"y":0,"z":0,"layer":0}})
-            sending.send(msg)
+    tokens = []
+    for quoted, bare in re.findall(r'"([^"]+)"|(\S+)', cmd):
+        tokens.append(quoted or bare)
+    cmd = tokens[0]
+    args = tokens[1:]
+    mod = conf.get("commands/commands.toml", cmd)
+    if mod in ("",{},[]):
+        client.send(f"Command: {cmd} not found.")
+        return
+    try:
+        module = il.load_module(f"config/commands/{mod.split('.')[0]}.py")
+        module.__getattribute__(mod.split('.')[1])(client=client,arguments=args)
+    except Exception as e:
+        client.send(f"Error loading command: {cmd}")
+        log.error(f"Error loading command: {cmd}:\n{e}",name=client.td)
+        return
 
 def play(client):
     while not client.disconnected:
