2275df0ca318c7813842bad2bdeb4b008c234d8e
zeviraty <zeviraty@gmail.com>
2026-01-24 17:24:11 +0100

    Added docstrings to config.py

--- src/tome/utils/config.py
+++ src/tome/utils/config.py
@@ -2,6 +2,16 @@ import toml
 import os
 
 def get(path:str, key:str="all"):
+    '''
+    Gets a key from a config file
+
+    Parameters
+    ----------
+    path : str
+        Path of the file
+    key : str, optional
+        Key to get, if set to all it gives all keys in a file (default is 'all')
+    '''
     if not path.endswith(".toml"):
         return {}
     y = toml.loads(open(os.path.join("config/",path), 'r').read())
@@ -14,7 +24,24 @@ def get(path:str, key:str="all"):
         except KeyError:
             return {}
 
-def get_dir(path:str, key:str="all", ls=False) -> list:
+def get_dir(path:str, key:str="all", ls: bool=False) -> list:
+    '''
+    Get all config files in a directory
+
+    Parameters
+    ----------
+    path : str
+        path of the folder
+    key : str, optional
+        key to get from all the files (default is 'all')
+    ls : bool, optional
+        Whether it should find files recursively (default is False)
+
+    Returns
+    -------
+    list
+        list of all files & keys
+    '''
     tmp = []
     for i in os.listdir(os.path.join("config/",path)):
         if ls == False:
