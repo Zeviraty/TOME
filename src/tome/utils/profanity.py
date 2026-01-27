08d4659bec0e5d92b4d4705de270173b93400b6c
zeviraty <zeviraty@gmail.com>
2026-01-27 09:16:19 +0100

    Added docstrings to profanity

--- src/tome/utils/profanity.py
+++ src/tome/utils/profanity.py
@@ -3,11 +3,30 @@ import re
 from . import logging as log
 
 class ProfanityChecker:
-    def __init__(self, profanity_dir="config/profanity") -> None:
+    '''
+    Class that checks profanity
+    '''
+    def __init__(self, profanity_dir: str = "config/profanity") -> None:
+        '''
+        Initializes a ProfanityChecker class
+
+        Parameters
+        ----------
+        profanity_dir : str, optional
+            Path to profanity dir (default is 'config/profanity')
+        '''
         self.profanity_set = set()
         self._load_profanity_words(profanity_dir)
 
     def _load_profanity_words(self, directory:str) -> None:
+        '''
+        Load words into a set
+
+        Parameters
+        ----------
+        directory : str
+            Path to profanity dir
+        '''
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
             if os.path.isfile(filepath):
@@ -18,10 +37,26 @@ class ProfanityChecker:
                             self.profanity_set.add(word)
 
     def contains_profanity(self, text:str) -> bool:
+        '''
+        Checks if the text contains profanity
+
+        Parameters
+        ----------
+        text : str
+            Text to check
+
+        Returns
+        -------
+        bool
+            Whether it contains profanity or not
+        '''
         words = re.findall(r"\b\w+\b", text.lower())
         return any(word in self.profanity_set for word in words)
 
 def init_checker():
+    '''
+    Initializes profanity checker
+    '''
     try:
         global pchecker
         pchecker = ProfanityChecker()
@@ -29,4 +64,17 @@ def init_checker():
         log.warn("FAILED TO INITIALIZE PROFANITY CHECKER",name="profanity")
 
 def check_profanity(text: str) -> bool:
+    '''
+    Checks if the text contains profanity
+
+    Parameters
+    ----------
+    text : str
+        Text to check
+
+    Returns
+    -------
+    bool
+        Whether it contains profanity or not
+    '''
     return pchecker.contains_profanity(text)
